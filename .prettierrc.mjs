1601b7c1837d0e0dcc9784c562491b19ac51090e
fm_dev_mx <frgmendoza@gmail.com>
2024-06-21 07:05:14 -0700

    refactor(config): Update Prettier, EditorConfig, and VSCode settings
        Ensure code consistency and formatting by updating Prettier and EditorConfig settings, and configuring VSCode preferences.
    
        ## Changes:
    
        ### Modified Files:
    
        - `.editorconfig`: Add comments to explain each configuration setting.
        - `.prettierrc.mjs`: Increase print width for better readability and consistent formatting.
        - `settings.json`: Configure VSCode to use Prettier with project settings, enable format on save, and ensure consistent code formatting for .astro files.

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -14,7 +14,8 @@ export default {
 	semi: true, // Añadir punto y coma al final de las declaraciones
 	singleQuote: false, // Usar comillas dobles en lugar de comillas simples
 	trailingComma: "all", // Añadir coma al final de los elementos en objetos y arrays
-	printWidth: 80, // Ajustar el ancho máximo de línea
+	printWidth: 100, // Ajustar el ancho máximo de línea
 	bracketSpacing: true, // Añadir espacios dentro de los corchetes de los objetos
 	arrowParens: "always", // Incluir paréntesis alrededor de argumentos únicos en funciones de flecha
+	bracketSameLine: false, // Añadir saltos de línea entre parentesis
 };
