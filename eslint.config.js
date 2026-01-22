ed63f77447a5961b38dc7a02860487da2affc6b4
Francisco Mendoza <frgmendoza@gmail.com>
2026-01-21 22:55:42 -0700

    chore(tooling): add flat eslint config

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import astroParser from 'astro-eslint-parser';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+
+export default [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...eslintPluginAstro.configs.recommended,
+    eslintPluginPrettierRecommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+    },
+    {
+        files: ['**/*.astro'],
+        languageOptions: {
+            parser: astroParser,
+            parserOptions: {
+                parser: tseslint.parser,
+                extraFileExtensions: ['.astro'],
+            },
+        },
+        rules: {
+            'astro/no-unused-css-selector': 'warn',
+        },
+    },
+    {
+        rules: {
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            'prettier/prettier': [
+                'error',
+                {
+                    endOfLine: 'auto',
+                    tabWidth: 4,
+                    useTabs: true,
+                },
+            ],
+        },
+    },
+    {
+        ignores: [
+            'dist/',
+            'node_modules/',
+            '.astro/',
+            '.vercel/',
+            'public/',
+            'package-lock.json',
+            'pnpm-lock.yaml',
+        ],
+    },
+];
