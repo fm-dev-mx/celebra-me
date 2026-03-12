5834ebdea045ac7fa2e46eaf3157e770ba8d6983
fm-dev-mx <48076635+fm-dev-mx@users.noreply.github.com>
2026-03-12 16:05:07 -0700

    feat(ui): register image assets for wedding demo

---
+++ src/assets/images/events/demo-wedding/index.ts
@@ -0,0 +1,22 @@
+import hero from './hero.png';
+import portrait from './portrait.png';
+import signature from './signature.png';
+import jardin from './jardin.png';
+import family from './family.png';
+import gallery01 from './gallery-01.png';
+import gallery02 from './gallery-02.png';
+import gallery03 from './gallery-03.png';
+import gallery04 from './gallery-04.png';
+import gallery05 from './gallery-05.png';
+import gallery06 from './gallery-06.png';
+
+export const assets = {
+	hero,
+	portrait,
+	ceremony: jardin,
+	reception: signature,
+	jardin,
+	signature,
+	family,
+	gallery: [gallery01, gallery02, gallery03, gallery04, gallery05, gallery06],
+};
