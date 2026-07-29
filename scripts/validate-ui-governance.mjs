f71ddafa4695c3e481ad581de0077914f0416aab
Francisco Mendoza <frgmendoza@gmail.com>
2026-07-28 18:43:22 -0700

    chore(governance): register ChangePasswordBehavior in hydrated-component allowlist

--- scripts/validate-ui-governance.mjs
+++ scripts/validate-ui-governance.mjs
@@ -26,6 +26,7 @@ const allowedHydratedComponents = new Set([
 	'DashboardUserMenu',
 	'MfaSetupBehavior',
 	'LoginFlowBehavior',
+	'ChangePasswordBehavior',
 	'RSVP',
 	'InvitationList',
 	'InvitationDetail',
